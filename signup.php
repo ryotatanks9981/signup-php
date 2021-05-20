61c676e2ddb32bbe3358d796d269269df48d6b9f
ryotatanks9981 <67944448+ryotatanks9981@users.noreply.github.com>
2021-05-20 20:51:31 +0900

    バリデーションの追加

--- signup.php
+++ signup.php
@@ -1,21 +1,32 @@
 <?php
 
+session_start();
+session_regenerate_id(true);
+
 $err[] = [];
 
-if ($username = filter_input(INPUT_POST, $_POST['username'])) {
-    $err[] = "";
+if (!$username = filter_input(INPUT_POST, 'username')) {
+    $err['username'] = "ユーザー名を入力してください";
 }
-?>
+if (!$email = filter_input(INPUT_POST, 'email')) {
+    $err['email'] = "メールアドレスを入力してください";
+}
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+$password = filter_input(INPUT_POST, 'password');
+$conf_password = filter_input(INPUT_POST, 'conf_password');
+
+if (!preg_match('/\A(?=.*?[^\w])(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])[!-~]{8,100}\z/', $password)) {
+    $err['password'] = "パスワードは大小英数字記号を最低1文字ずつ含んだ8~100文字以内にしてください";
+}
+
+if ($password !== $conf_password) {
+    $err['conf_password'] = "パスワードが一致しません";
+}
+
+if (count($err) !== 0) {
+    $_SESSION['err'] = $err;
+    header("Location: signup_form.php");
+    exit();
+} else {
+    //新規登録処理
+}
